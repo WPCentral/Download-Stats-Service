0f581bc3cbc0f7138687c19fc59cfe62d9e3b0b7
Marko Heijnen <info@markoheijnen.com>
2015-12-22 14:35:21 +0100

    Add route for latest download count for a version

--- server.js
+++ server.js
@@ -20,6 +20,27 @@ pool.on('enqueue', function () {
 	log_error('Waiting for available connection slot');
 });
 
+app.get( '/count/:version', function(req, res) {
+	pool.getConnection(function(err, connection) {
+		var sql     = "SELECT * FROM downloads WHERE version = ? ORDER BY count DESC LIMIT 1";
+		var inserts = [ req.params.version ];
+		sql         = mysql.format(sql, inserts);
+
+		connection.query( sql, function(err, rows, fields) {
+			if ( ! err && rows.length > 0 ) {
+				res.json(rows[0]);
+			}
+			else {
+				res.json({
+					'count': 0
+				});
+			}
+
+			connection.release();
+		});
+	});
+});
+
 app.use(function(req, res, next) {
 	res.status(404).json({
 		'error': "Route doesn;'t exist"
